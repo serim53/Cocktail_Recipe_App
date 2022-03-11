66d3b269d332fa519e942997501e5bf9e4fa332f
serim53 <tpfla9953@naver.com>
2022-03-11 23:31:15 +0900

    add viewpager2, bottomnavigation

--- app/src/main/java/com/example/cocktailrecipe/MainActivity.kt
+++ app/src/main/java/com/example/cocktailrecipe/MainActivity.kt
@@ -5,73 +5,53 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager2.widget.ViewPager2
 import com.example.cocktailrecipe.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     lateinit var mainBinding: ActivityMainBinding
 
-    private lateinit var service: CocktailService
-
-    private val adapter by lazy {
-        CocktailAdapter()
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
-    }
+        mainBinding.viewpager.adapter = ViewPagerAdapter(this)
 
-    override fun onStart() {
-        super.onStart()
-
-        initViews()
-
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://www.thecocktaildb.com/api/json/v1/1/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        service = retrofit.create(CocktailService::class.java)
-
-        mainBinding.buttonSearch.setOnClickListener {
-            searchCocktail()
-        }
-    }
-
-    private fun initViews() {
-        mainBinding.recyclerView.layoutManager = LinearLayoutManager(applicationContext)
-        mainBinding.recyclerView.adapter =  adapter
-    }
-
-    private fun searchCocktail() {
-        service.listCocktails(mainBinding.edittextSearch.text.toString()).enqueue(
-            object: Callback<Cocktail> {
-                override fun onResponse(
-                    call: Call<Cocktail>,
-                    response: Response<Cocktail>
-                ) {
-                    response.body()?.let{
-                        Log.d("retrofit_test", it.drinks.toString())
-                        if (it.drinks.isNullOrEmpty()){
-                            Toast.makeText(this@MainActivity, "정보가 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
-                        } else {
-                            adapter.setData(it.drinks)
-                        }
+        mainBinding.viewpager.registerOnPageChangeCallback(
+            object: ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    mainBinding.bottomNavigationView.menu.getItem(position).isChecked = true
+                }
+            }
+        )
+        mainBinding.bottomNavigationView.run {
+            setOnItemSelectedListener { item ->
+                when(item.itemId) {
+                    R.id.item_popular -> {
+                        mainBinding.viewpager.currentItem = 0
+                        true
                     }
+                    R.id.item_search -> {
+                        mainBinding.viewpager.currentItem = 1
+                        true
+                    }
+                    R.id.item_favorite -> {
+                        mainBinding.viewpager.currentItem = 2
+                        true
+                    }
+                    else -> false
                 }
-
-                override fun onFailure(call: Call<Cocktail>, t: Throwable) {
-                    t.printStackTrace()
-                }
-
-            })
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/cocktailrecipe/SearchFragment.kt
@@ -0,0 +1,81 @@
+package com.example.cocktailrecipe
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cocktailrecipe.databinding.FragmentSearchBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class SearchFragment : Fragment() {
+
+    lateinit var fragmentSearchBinding: FragmentSearchBinding
+    private lateinit var service: CocktailService
+    private val adapter by lazy {
+        CocktailAdapter()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        fragmentSearchBinding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+        return fragmentSearchBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.thecocktaildb.com/api/json/v1/1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        service = retrofit.create(CocktailService::class.java)
+
+        fragmentSearchBinding.buttonSearch.setOnClickListener {
+            searchCocktail()
+        }
+    }
+
+    private fun initViews() {
+        fragmentSearchBinding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        fragmentSearchBinding.recyclerView.adapter =  adapter
+    }
+
+    private fun searchCocktail() {
+        service.listCocktails(fragmentSearchBinding.edittextSearch.text.toString()).enqueue(
+            object: Callback<Cocktail> {
+                override fun onResponse(
+                    call: Call<Cocktail>,
+                    response: Response<Cocktail>
+                ) {
+                    response.body()?.let{
+                        Log.d("retrofit_test", it.drinks.toString())
+                        if (it.drinks.isNullOrEmpty()){
+                            Toast.makeText(requireContext(), "정보가 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
+                        } else {
+                            adapter.setData(it.drinks)
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<Cocktail>, t: Throwable) {
+                    t.printStackTrace()
+                }
+
+            })
+    }
+}
